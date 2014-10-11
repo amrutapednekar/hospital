516f620752f51901b79eb38758f3315e6f214519
amrutapednekar <amruta.jadhav1@gmail.com>
2014-10-12 01:56:39 +0200

    removed ruby version from environmet.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ruby ENV['CUSTOM_RUBY_VERSION'] || '1.9.3'
+#ruby ENV['CUSTOM_RUBY_VERSION'] || '1.9.3'
 
